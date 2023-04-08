00c5c3db9165427dd818e75f5208db29fa5ceaaa
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2023-04-08 18:32:56 +0000

    Initial Commit

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,10 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
 import { BiLogOut } from 'react-icons/bi';
+
 import SidebarLogo from './SidebarLogo';
 import SidebarItem from './SidebarItem';
+import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
   const items = [
@@ -36,6 +38,7 @@ const Sidebar = () => {
             />
           ))}
           <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          <SidebarTweetButton />
         </div>
       </div>
     </div>
---
+++ components/layout/SidebarTweetButton.tsx
@@ -0,0 +1,5 @@
+const SidebarTweetButton = () => {
+  return <div></div>;
+};
+
+export default SidebarTweetButton;
