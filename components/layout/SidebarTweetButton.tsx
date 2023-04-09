3901fcc2c7a4cd1a45fcf18e840fb85d8674651b
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2023-04-09 05:47:36 +0000

    Initial Commit

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,5 +1,15 @@
+import { useRouter } from 'next/router';
+import { FaFeather } from 'react-icons/fa';
+
 const SidebarTweetButton = () => {
-  return <div></div>;
+  const router = useRouter();
+  return (
+    <div onClick={() => router.push('/')}>
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
+        <FaFeather size={24} color="white" />
+      </div>
+    </div>
+  );
 };
 
 export default SidebarTweetButton;
