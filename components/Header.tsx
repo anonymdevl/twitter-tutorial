143929298b5ade5288458dfa781351212d4af735
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2023-04-12 17:18:42 +0000

    Modified header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
   const router = useRouter();
+
   const handleBack = useCallback(() => {
     router.back();
   }, [router]);
